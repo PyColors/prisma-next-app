0d2ec75a42481b5c4b60b8eac9eba3590c000fb0
PyColors <patweb44@gmail.com>
2021-04-10 14:53:23 +0200

    Update the API route with deletePost

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -30,6 +30,13 @@ async function publishPost(id: number): Promise<void> {
   await Router.push("/");
 }
 
+async function deletePost(id: number): Promise<void> {
+  await fetch(`http://localhost:3000/api/post/${id}`, {
+    method: "DELETE"
+  });
+  Router.push("/");
+}
+
 const Post: React.FC<PostProps> = props => {
   const [session, loading] = useSession();
   if (loading) {
